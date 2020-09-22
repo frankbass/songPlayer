04357159f5ebcb834c5c665f19c3aeec0ebc42d6
frankbass <36844912+frankbass@users.noreply.github.com>
2020-09-22 19:31:06 +0200

    audio name change

--- sketch.js
+++ sketch.js
@@ -1,5 +1,18 @@
+//attach a click listener to a play button
+document.querySelector('button')?.addEventListener('click', async () => {
+	await Tone.start()
+	console.log('audio is ready')
+})
+
+const player = new Tone.Player("/audio/03 Moanin'.mp3").toMaster();
+// const player = new Tone.Player("https://tonejs.github.io/audio/berklee/gong_1.mp3").toDestination();
+// play as soon as the buffer is loaded
+player.autostart = true;
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	createCanvas(200, 200);
+	fill(0);
+	rect(10,10, 100,100);
 }
 
 function draw() {
