dea4fc6cf0b77fad9d1d4cdcf8b6c729a8dd80bc
frankbass <36844912+frankbass@users.noreply.github.com>
2020-09-23 08:56:00 +0200

    renamed song

--- sketch.js
+++ sketch.js
@@ -4,10 +4,11 @@ document.querySelector('button')?.addEventListener('click', async () => {
 	console.log('audio is ready')
 })
 
-const player = new Tone.Player("/audio/03 Moanin'.mp3").toMaster();
+const player = new Tone.Player("audio/Moanin.mp3").toDestination();
+// const player = new Tone.Player("https://github.com/frankbass/songPlayer/blob/master/audio/Moanin.mp3").toMaster();
 // const player = new Tone.Player("https://tonejs.github.io/audio/berklee/gong_1.mp3").toDestination();
 // play as soon as the buffer is loaded
-player.autostart = true;
+// player.autostart = true;
 
 function setup() {
 	createCanvas(200, 200);
@@ -17,4 +18,14 @@ function setup() {
 
 function draw() {
 
+}
+
+var play = 0;
+var time = 0;
+function mousePressed() {
+	play ++;
+	play = play % 2;
+	if (play) {player.start();}
+	else {player.stop()}
+	console.log(player.state);
 }
